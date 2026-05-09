eba3aea5aa404169f01e9232e8cbbe1ad1b63092
jabieger <139219572+jabieger@users.noreply.github.com>
2026-05-09 19:05:17 +0200

    Create sources.py

---
+++ sources.py
@@ -0,0 +1,20 @@
+from pyscript.web import page, a
+
+def links_hinzufuegen_zu(target):  # target ist die id des Ziel-divs
+    div = page[target]
+
+    div.append(
+        a(
+            "Wikipedia",
+            href="https://de.wikipedia.org",
+            target="_blank"
+        )
+    )
+
+    div.append(
+        a(
+            "YouTube",
+            href="https://www.youtube.com",
+            target="_blank"
+        )
+    )
